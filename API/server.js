216a33a7e997b47d829d179212b151e839d923d1
Vinicin <vserafim_o@outlook.com>
2023-09-22 15:26:20 -0300

    glória a deus

---
+++ API/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const eureka = require('./eurekaClient');
+
+const GATEWAY_ADDRESS = process.env.ADDRESS || 'localhost';
+const GATEWAY_PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.json("I am user-service")
+})
+
+app.listen(GATEWAY_PORT, () => {
+    console.log(`Gateway running on ${GATEWAY_ADDRESS}:${GATEWAY_PORT}`);
+})
+  
+
+eureka.registerWithEureka('express-gateway-service', GATEWAY_PORT);
---
+++ eureka/src/test/java/com/deal/savage/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deal.savage.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
